7828bd89aa539bc8b2fdf011884cad1fde6ad058
Maxime J. <jennmaxime@gmail.com>
2025-04-10 20:52:13 +0200

    Intro

---
+++ src/components/containers/TextContainer.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const TextContainer = styled.div`
+    width: 100%;
+    padding: 20px 40px;
+    font-weight: 600;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    box-sizing: border-box;
+    gap: 20px;
+    line-height: 1.3;
+    overflow-y: auto;
+`
